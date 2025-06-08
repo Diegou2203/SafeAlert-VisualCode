e632fc7aa696d39cfd66466c6b886690c5b1dca5
Diegou2203 <diegouribe990@gmail.com>
2025-06-07 23:42:04 -0500

    Listar Ubicaciones

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ListarusuarioComponent } from './listarusuario/listarusuario.component';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { ListarrolComponent } from "./listarrol/listarrol.component";
+import { ListarubicacionComponent } from './listarubicacion/listarubicacion.component';
 
 @Component({
   selector: 'app-usuario',
-  imports: [ListarusuarioComponent, RouterOutlet, ListarrolComponent],
+  imports: [ListarusuarioComponent, RouterOutlet, ListarrolComponent, ListarubicacionComponent],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
 })
--- src/app/services/ubicacion.service.ts
+++ src/app/services/ubicacion.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environments';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Usuario } from '../models/usuario';
+import { HttpClient } from '@angular/common/http';
+import { Ubicacion } from '../models/ubicacion';
 const base_url=environment.base
 @Injectable({
   providedIn: 'root'
 })
-export class UsuarioService {
-
-  private url=`${base_url}/usuarios`
+export class UbicacionService {
+  private url=`${base_url}/ubicaciones`
 
   constructor(private http:HttpClient) { }
-  
+
   list(){
-    return this.http.get<Usuario[]>(this.url+'/list')
+    return this.http.get<Ubicacion[]>(this.url+'/list')
   }
+
 }
