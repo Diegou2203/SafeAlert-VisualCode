137ba7c75cdde571538ebcdfc7bfd2e69f712b33
eloydadaea <57968579+eloydadaea@users.noreply.github.com>
2025-06-10 00:31:02 -0500

    hola

---
+++ src/app/models/TipoFenomeno.ts
@@ -0,0 +1,7 @@
+import { FenomenoNatural } from './FenomenoNatural';
+export class TipoFenomeno {
+  idTipoFenomeno: number = 0;
+  nombre_tipo: string = '';
+  descripcion: string = '';
+  fenomenoNatural: FenomenoNatural[] = [];
+}
