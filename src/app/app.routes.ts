9ac0d565ceb5028ae1ff367413034d99693b3770
EloyAlata <eloy.alatago@udlap.mx>
2025-06-12 06:09:04 -0500

    insertandedituser

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,18 @@
 import { Routes } from '@angular/router';
-import { ListarusuarioComponent } from './components/usuario/listarusuario/listarusuario.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import path from 'path';
+import { Insertareditarusuario } from './components/usuario/insertareditarusuario/insertareditarusuario';
 
 export const routes: Routes = [
+    {
+        path:"aplicaciones", component:UsuarioComponent,
+        children:[
+            {
+                 path: "RegistrarUser", component:Insertareditarusuario
+            }
+
+        ]
+    }
+
+    
 ];
